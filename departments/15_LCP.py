5204ee9dbeacc3b1de1db6c148b450113951b922
Epsita-17 <anee.epsita81@gmail.com>
2026-09-26 14:42:07 +0530

    Update 15_LCP.py

--- departments/15_LCP.py
+++ departments/15_LCP.py
@@ -1954,9 +1954,10 @@ with b:
                 "ID"
             ],
             [
+                "Name of PHA",
+                "PHA Name",
                 "PHA Description",
-                "Description",
-                "PHA Name"
+                "Description"
             ],
             [
                 "Status",
