0b080d0d5e462b0ad64fdb95320da4e17d70bd3f
Epsita-17 <anee.epsita81@gmail.com>
2026-09-26 14:36:50 +0530

    Update 04_SMS_1.py

--- departments/04_SMS_1.py
+++ departments/04_SMS_1.py
@@ -1941,9 +1941,10 @@ with b:
                 "ID"
             ],
             [
+                "Name of PHA",
+                "PHA Name",
                 "PHA Description",
-                "Description",
-                "PHA Name"
+                "Description"
             ],
             [
                 "Status",
