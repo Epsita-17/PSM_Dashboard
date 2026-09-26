1322a788e10961c99e7928f835add114b1ec5313
Epsita-17 <anee.epsita81@gmail.com>
2026-09-26 14:35:54 +0530

    Update 01_Blast_Furnace.py

--- departments/01_Blast_Furnace.py
+++ departments/01_Blast_Furnace.py
@@ -1963,9 +1963,10 @@ with b:
                 "ID"
             ],
             [
+                "Name of PHA",
+                "PHA Name",
                 "PHA Description",
-                "Description",
-                "PHA Name"
+                "Description"
             ],
             [
                 "Status",
