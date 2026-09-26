f8828c0e8bd1c3c9f2211542b9e715ce364fb9a1
Epsita-17 <anee.epsita81@gmail.com>
2026-09-26 14:39:59 +0530

    Update 09_WRM.py

--- departments/09_WRM.py
+++ departments/09_WRM.py
@@ -1955,9 +1955,10 @@ with b:
                 "ID"
             ],
             [
+                "Name of PHA",
+                "PHA Name",
                 "PHA Description",
-                "Description",
-                "PHA Name"
+                "Description"
             ],
             [
                 "Status",
